d5968170194fd5ca64948cc571c5f8fdc86da779
Jeff Lee <jeffyuanhuanlee@gmail.com>
2021-01-14 16:48:05 -0500

    i did stuff

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# bluduck-be
+# BluDuck Web Service
+
+## Getting Started
+
+BluDuck web service
+
+In Git Bash, navigate to project root and type
+
+```
+./init
+```
+
+This will spin up a dockerized container to run on *http://localhost*
+
+## API Documentation
+
+Swagger all up in your bitch ass.
+
+http://localhost/api/docs
--- src/swagger/constants.ts
+++ src/swagger/constants.ts
@@ -1,4 +1,4 @@
 export const SWAGGER_API_ROOT = 'api/docs';
-export const SWAGGER_API_NAME = 'Simple API';
-export const SWAGGER_API_DESCRIPTION = 'Simple API Description';
+export const SWAGGER_API_NAME = 'BluCuck API';
+export const SWAGGER_API_DESCRIPTION = 'BluCuck API Service for make benefit of cucky consumption of data';
 export const SWAGGER_API_CURRENT_VERSION = '1.0';
